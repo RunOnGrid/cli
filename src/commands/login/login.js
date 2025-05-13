d1dfc5c9196ab2d372d279a12742102dd985fb8f
Benjamin Aguirre <benjaminaguirre@Mac.lan>
2025-05-13 09:23:38 -1000

    improve readme.md for npm documentation

--- src/commands/login/login.js
+++ src/commands/login/login.js
@@ -5,45 +5,49 @@ import { logInOAuth, gridLogin } from "../../service/authServer.js";
 
 export const login = new Command("login")
   .description("Log in using email/password or google/github")
-  .option("--google", "Log in with Google")
-  .option("--github", "Log in with github")
-  .action(async (options) => {
-    if (options.google) {
-      await logInOAuth("google");
-      return;
-    }if(options.github){
-      await logInOAuth("github");
-      return;
-    }
-     else {
-      await loginWithUser();
-      return;
-    }
-  });
 
-async function loginWithUser() {
-  const answers = await inquirer.prompt([
-    {
-      name: "email",
-      type: "input",
-      message: "Email:",
-      default() {
-        return "Email";
-      },
-    },
-    {
-      name: "password",
-      type: "password",
-      message: "Password:",
-      default() {
-        return "Password";
-      },
-    },
-  ]);
 
-  const response = await gridLogin(answers.email, answers.password);
-  console.log(response);
+
+const googlelog = new Command("google")
+.description("Access Grid via google authentication")
+.action(async ()=>{
+    await logInOAuth("google");
+    return;
+})
+const githublog = new Command("github")
+.description("Access Grid via github authentication")
+.action(async ()=>{
+    await logInOAuth("github");
+    return;
+})
+
+login.addCommand(googlelog);
+login.addCommand(githublog);
+
+
+// async function loginWithUser() {
+//   const answers = await inquirer.prompt([
+//     {
+//       name: "email",
+//       type: "input",
+//       message: "Email:",
+//       default() {
+//         return "Email";
+//       },
+//     },
+//     {
+//       name: "password",
+//       type: "password",
+//       message: "Password:",
+//       default() {
+//         return "Password";
+//       },
+//     },
+//   ]);
+
+//   const response = await gridLogin(answers.email, answers.password);
+//   console.log(response);
   
-  await saveToken(response);
-  console.log("Token saved");
-}
+//   await saveToken(response);
+//   console.log("Token saved");
+// }
